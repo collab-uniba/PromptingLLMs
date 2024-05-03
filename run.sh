2c3911943b12e1b8882da2a21a126aeccde1a17b
Giuseppe Colavito <gcolavit@login02.leonardo.local>
2024-05-03 15:36:23 +0200

    add run scripts for hpc

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -A IscrC_LLM4SE
+#SBATCH -p boost_usr_prod
+#SBATCH --qos boost_qos_dbg
+#SBATCH --time 0-00:30:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:2
+#SBATCH --mem=246000
+#SBATCH --job-name=llm4se_inference
+#SBATCH --out=llm4se_inference.out
+#SBATCH --err=llm4se_inference.out
+
+srun ./predict_exe
